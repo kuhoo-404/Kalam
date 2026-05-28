053f9e87d16cc4827e1eddc564b08eb33f320eb2
kuhoo_404 <kuhooshinde05@gmail.com>
2026-05-28 08:45:28 +0530

    fix: point frontend to production backend

--- frontend/src/utilis/api.js
+++ frontend/src/utilis/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000',
+  baseURL: 'https://kalam-backend-plyr.onrender.com',
 });
 
 // Auto-attach token to every request
